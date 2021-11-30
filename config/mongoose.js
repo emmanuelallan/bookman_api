1683f7a3c2ce591d8c0a14e02ec7b70de6eeba58
maxthestranger <thestrangermax@gmail.com>
2021-11-30 11:29:35 +0300

    chore: mongoose setup

---
+++ config/mongoose.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const logger = require('./logger');
+const { mongo, env } = require('./vars');
+
+// set mognoose Promise to bluebird
+mongoose.Promise = Promise;
+
+// exit application on error
+mongoose.connection.on('error', (err) => {
+  logger.error(`MongoDB connection error: ${err}`);
+  process.exit(-1);
+});
+
+// print mongoose logs in dev env
+if (env === 'development') {
+  mongoose.set('debug', true);
+}
+
+// connect to mongo db
+exports.connect = () => {
+  mongoose
+    .connect(mongo.uri, {
+      useCreateIndex: true,
+      keepAlive: 1,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log('mongoDB connected...'));
+  return mongoose.connection;
+};
