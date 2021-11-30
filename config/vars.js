3b1d1f90aacc3c00bac8ed8a58f0b6a9e88ac725
maxthestranger <thestrangermax@gmail.com>
2021-11-30 11:41:14 +0300

    chore: nodemon

--- config/vars.js
+++ config/vars.js
@@ -10,10 +10,7 @@ module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   mongo: {
-    uri:
-      process.env.NODE_ENV === 'test'
-        ? process.env.MONGO_URI_TESTS
-        : process.env.MONGO_URI,
+    uri: process.env.MONGO_URI,
   },
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
 };
